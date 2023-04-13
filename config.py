51f14578fa956822dc3fa2833d13e970610b4714
jytan0110 <130631185+jytan0110@users.noreply.github.com>
2023-04-13 12:44:47 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "database-1.cnqjbll5yosk.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "tanjieyi-employee"
+customregion = "us-east-1"
